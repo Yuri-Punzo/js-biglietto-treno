2173625c45da979332b3bbeb33b7a7df34819e4b
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-06 13:52:14 +0200

    first 2 prompts

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -9,3 +9,9 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
     - va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 Questo richiederà un minimo di ricerca, oltre alla ricerca da fare per ricordarsi come calcolare uno sconto (Googlate qulcosa del tipo: come si calcola lo sconto? e cercate una risorsa che vi possa aiutare a capire cosa fare) */
+
+const howManyKm = prompt("Quanti km desideri percorrere ?");
+console.log(howManyKm);
+
+const userAge = prompt("Quanti anni hai ?");
+console.log(userAge);
