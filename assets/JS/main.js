410c59cab448916fb91d28c98d453f5e713f3011
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-06 14:54:16 +0200

    all js finished + some bootstrap for aesthetic

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -6,7 +6,7 @@ Il programma dovrà chiedere all'utente:
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
     - il prezzo del biglietto è definito in base ai km (0.21 € al km)
     - va applicato uno sconto del 20% per i minorenni
-    - va applicato uno sconto del 40% per gli over 65.
+    - va applicato uno sconto del 40% per gli ove   r 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 Questo richiederà un minimo di ricerca, oltre alla ricerca da fare per ricordarsi come calcolare uno sconto (Googlate qulcosa del tipo: come si calcola lo sconto? e cercate una risorsa che vi possa aiutare a capire cosa fare) */
 
@@ -15,3 +15,22 @@ console.log(howManyKm);
 
 const userAge = prompt("Quanti anni hai ?");
 console.log(userAge);
+
+const generalPrice = 0.21;
+const minorPrice = 0.168;
+const elderlyPrice = 0.126;
+
+if (userAge <= 17) {
+    let finalPrice = howManyKm * minorPrice
+    console.log(finalPrice.toFixed( 2 ));
+    document.getElementById("trip_price").innerHTML = `${finalPrice.toFixed( 2 )}`
+} else if (userAge >= 65) {
+    let finalPrice = howManyKm * elderlyPrice
+    console.log(finalPrice.toFixed( 2 ));
+    document.getElementById("trip_price").innerHTML = `${finalPrice.toFixed( 2 )}`
+} else {
+    let finalPrice = howManyKm * generalPrice
+    console.log(finalPrice.toFixed( 2 ));
+    document.getElementById("trip_price").innerHTML = `${finalPrice.toFixed( 2 )}`
+}
+
